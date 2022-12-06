1e7a172abc36fa6370eabea7895ef70382201d09
vlad-000-236 <102834644+vlad-000-236@users.noreply.github.com>
2022-12-06 17:39:34 +0800

    Update Game.java

--- src/main/java/com/mycompany/croossandnought/Game.java
+++ src/main/java/com/mycompany/croossandnought/Game.java
@@ -1,11 +1,4 @@
 package com.mycompany.croossandnought;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author vlad__236
